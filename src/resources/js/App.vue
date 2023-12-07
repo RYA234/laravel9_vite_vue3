0088c7a4f9ed3dcfc1883e26cc6e22c01788ae95
RYA234 <mryuabc@gmail.com>
2023-12-07 21:35:33 +0900

    step2完了

--- src/resources/js/App.vue
+++ src/resources/js/App.vue
@@ -1,18 +1,15 @@
+<script setup>
+import { reactive, ref } from 'vue'
+
+// step2
+const counter = reactive({ count: 0 })
+const message = ref('Hello World!')
+console.log(message);
+</script>
+
 <template>
-    <div class="page">
-      <p>{{ counter }}</p>
-      <button @click="counter += 1">
-        click!
-      </button>
-    </div>
-   </template>
-  
-   <script>
-   export default {
-    data () {
-      return {
-        counter: 0,
-      }
-    },
-   }
-   </script>
+<!-- step2 -->
+  <h1>{{ message }}</h1>
+  <h1>{{ message }}</h1>
+  <p>Count is: {{ counter.count }}</p>
+</template>
