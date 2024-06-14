940406e2a922fa687595bb2196d1cbd4d00f295a
RYA234 <103587568+RYA234@users.noreply.github.com>
2024-06-14 23:01:41 +0000

    POST METHODが呼び出されることとリクエストボディは確認 レスポンスは確認していない

--- src/resources/js/component/Signup.test.js
+++ src/resources/js/component/Signup.test.js
@@ -1,8 +1,13 @@
+//vitest の機能
+// 外部のライブラリ使うときは宣言しておく？？
+vi.mock('axios')
 import { render, screen } from '@testing-library/vue';
 import { describe, expect, it } from 'vitest';
 import Signup from './Signup.vue';
 import userEvent from '@testing-library/user-event'
-//
+import axios from 'axios';
+
+
 describe('ユーザー作成画面', () => {
     it('ヘッダーが存在しているか', () => {
       render(Signup)
@@ -44,12 +49,36 @@ describe('ユーザー作成画面', () => {
         render(Signup)
         expect(screen.getByRole('button',{ name: 'Sign Up'})).toBeDisabled();
     })
+})
 
+describe('ユーザー作成API',()=>{
+    it('正しい値を入力したうえで、ボタンを押すとURIにpostMethodが送られる',async()=>{
+        const user = userEvent.setup();
+        render(Signup);
 
+        // testingライブラリから要素を取得する
+        const usernameInput = screen.getByLabelText('Username');
+        const emailInput = screen.getByLabelText('E-mail');
+        const passwordInput = screen.getByLabelText('Password');
+        const passwordRepeatInput = screen.getByLabelText('PasswordRepeat');
 
+        // testing library/user-eventを使ってフォームに値を入力する
+        // https://testing-library.com/docs/user-event/v13/#typeelement-text-options
+        await user.type(usernameInput,'user1');
+        await user.type(emailInput,'user@mail.com');
+        await user.type(passwordInput,'P4ssword');
+        await user.type(passwordRepeatInput,'P4ssword');
+        const button = screen.getByRole('button',{name:'Sign Up'});
 
+        await user.click(button);
 
+        expect(axios.post).toHaveBeenCalledWith('/api/v1/user',{
+            username:'user1',
+            email:'user@mail.com',
+            password:'P4ssword'
+        })
 
+    })
 
 
 })
--- src/resources/js/component/Signup.vue
+++ src/resources/js/component/Signup.vue
@@ -1,9 +1,21 @@
 <script setup>
-import { computed,ref} from 'vue';
-// const disabled = ref(true)
+import axios from 'axios';
+import { computed, ref } from 'vue';
+
 const password = ref('')
 const passwordRepeat = ref('')
+const username = ref('')
+const email = ref('')
 
+
+const submit = () => {
+    console.log('submit')
+    axios.post('/api/v1/user', {
+        username: username.value,
+        email: email.value,
+        password: password.value
+    })
+}
 const isDisabled = computed(() => {
     return password.value || passwordRepeat.value ? password.value !== passwordRepeat.value : true
 })
@@ -13,6 +25,14 @@ const isDisabled = computed(() => {
 <template>
 <h1>SignUp</h1>
 <div>
+    <label for="username">Username</label>
+    <input type="text" id="username" v-model="username"/>
+</div>
+<div>
+    <label for="email">E-mail</label>
+    <input type="text" id="email" v-model="email"/>
+</div>
+<div>
     <label for="password">Password</label>
     <input type="password" id="password" v-model="password"/>
 </div>
@@ -20,7 +40,5 @@ const isDisabled = computed(() => {
     <label for="passwordRepeat">PasswordRepeat</label>
     <input type="password" id="passwordRepeat" v-model="passwordRepeat"/>
 </div>
-    <button :disabled=" isDisabled" >Sign Up</button>
-
-
+    <button :disabled=" isDisabled" @click="submit" >Sign Up</button>
 </template>
