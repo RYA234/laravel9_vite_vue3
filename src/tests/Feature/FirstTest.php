aa6e5f6fe448afc703bf6a5c1f02864df99c3eae
RYA234 <103587568+RYA234@users.noreply.github.com>
2024-06-25 05:17:55 +0000

    13 14 assertSeeとステータスコード

---
+++ src/tests/Feature/FirstTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class FirstTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    function test_sample12(){
+        $response = $this->get('/');
+        $response->assertStatus(200)
+                 ->assertSee('Laravel')
+                 ->assertSee(['Laravel', 'Vite', 'Vue'])
+                 ->assertSee('<h1>Hello,権兵衛さん</h1>',false);
+
+    }
+}
