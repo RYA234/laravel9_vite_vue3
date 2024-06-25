10f69b94cf68a06b09f789207982584ebb5e8493
RYA234 <103587568+RYA234@users.noreply.github.com>
2024-06-25 04:05:38 +0000

    Emitter step1

---
+++ src/resources/js/component/testUtils/Emitter.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+    </div>
+  </template>
+
+  <script>
+    import { defineComponent } from 'vue';
+    export default defineComponent({
+        props: {
+        },
+        setup(props,context){
+
+        const emitEvent =()=>{
+            context.emit("myEvent","name","password")
+        }
+        return {
+                emitEvent
+            }
+        }
+    })
+
+  </script>
+
+  <style scoped>
+  </style>
