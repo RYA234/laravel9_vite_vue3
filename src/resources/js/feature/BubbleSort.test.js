c9c2c46b6f9da2ce4a8506fd5a2b79bff9f12d1a
RYA234 <103587568+RYA234@users.noreply.github.com>
2024-07-16 21:28:51 +0000

    バリデーション未だ

--- src/resources/js/feature/BubbleSort.test.js
+++ src/resources/js/feature/BubbleSort.test.js
@@ -1,12 +1,13 @@
 
-import { mount, shallowMount } from '@vue/test-utils'
+import { mount, shallowMount,findComponents } from '@vue/test-utils'
 // import {render, screen} from '@testing-library/vue'
-import { test } from 'vitest'
+import { expect, test } from 'vitest'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import BubbleSort from './BubbleSort.vue'
-
+import flushPromises from "flush-promises"
+import { defineComponent } from 'vue'
 
 // docker-compose exec -it app bash -c  "npm run test BubbleSort.test.js "
 
@@ -17,7 +18,7 @@ const vuetify = createVuetify({
 
 global.ResizeObserver = require('resize-observer-polyfill')
 
-test('displays message', () => {
+test('アルゴリズムの単体テスト', () => {
 
   const wrapper = mount(BubbleSort,
     {
@@ -25,10 +26,85 @@ test('displays message', () => {
       plugins: [vuetify],
     }
   })
-
-// const wrapper = mount(BubbleSort);
-  console.log(wrapper.html())
-//   console.log(wrapper.findComponent(components.VBtn).html())
-  // Assert the rendered text of the component
-  //   expect(wrapper.text()).toContain('Components')
+  //   console.log(wrapper.html())
+  expect(wrapper.vm.bubbleSort([81,100,4,5])).toMatchObject([4,5,81,100])
+  // 文字列が入力された場合でも数値に変換してソートすることを確認する
+  expect(wrapper.vm.bubbleSort(['1','16','8','2'])).toMatchObject([1,2,8,16])
 })
+
+
+  test('初期表示', () => {
+
+    const wrapper = mount(BubbleSort,
+      {
+      global: {
+        plugins: [vuetify],
+      }
+    })
+    // テキストボックスを表示
+    expect(wrapper.findAllComponents(components.VTextField).at(0).text()).toBe("")
+    expect(wrapper.findAllComponents(components.VTextField).at(1).text()).toBe("")
+    expect(wrapper.findAllComponents(components.VTextField).at(2).text()).toBe("")
+    expect(wrapper.findAllComponents(components.VTextField).at(3).text()).toBe("")
+
+    // 結果欄は表示されない
+    expect(wrapper).not.toContain('output_1')
+    expect(wrapper).not.toContain('output_2')
+    expect(wrapper).not.toContain('output_3')
+    expect(wrapper).not.toContain('output_4')
+
+  })
+
+  test('計算ボタンを押して、結果が表示される', async() => {
+    const wrapper = mount(BubbleSort,
+      {
+      global: {
+        plugins: [vuetify],
+      }
+    })
+
+    // console.log(wrapper.find('#input_1').html())
+    // 入力
+    await wrapper.find('#input_1').setValue('16')
+    await wrapper.find('#input_2').setValue('2')
+    await wrapper.find('#input_3').setValue('1')
+    await wrapper.find('#input_4').setValue('8')
+    // 計算実行
+    await wrapper.find('#submit').trigger('click');
+
+    // 結果確認
+    expect(wrapper.find('#output_1').html()).toContain('value="1"')
+    expect(wrapper.find('#output_2').html()).toContain('value="2"')
+    expect(wrapper.find('#output_3').html()).toContain('value="8"')
+    expect(wrapper.find('#output_4').html()).toContain('value="16"')
+  })
+
+test('リセットボタンを押して初期化する',async() =>{
+    const wrapper = mount(BubbleSort,
+        {
+        global: {
+          plugins: [vuetify],
+        }
+      })
+
+    // 初期状態　入力して
+    await wrapper.find('#input_1').setValue('8')
+    await wrapper.find('#input_2').setValue('5')
+    await wrapper.find('#input_3').setValue('88')
+    await wrapper.find('#input_4').setValue('77')
+    await wrapper.find('#submit').trigger('click');
+
+    await wrapper.find('#reset').trigger('click');
+
+    //入力値の確認
+    expect(wrapper.findAllComponents(components.VTextField).at(0).text()).toBe("")
+    expect(wrapper.findAllComponents(components.VTextField).at(1).text()).toBe("")
+    expect(wrapper.findAllComponents(components.VTextField).at(2).text()).toBe("")
+    expect(wrapper.findAllComponents(components.VTextField).at(3).text()).toBe("")
+    // 結果欄は表示されない
+    expect(wrapper).not.toContain('output_1')
+    expect(wrapper).not.toContain('output_2')
+    expect(wrapper).not.toContain('output_3')
+    expect(wrapper).not.toContain('output_4')
+})
+
--- src/resources/js/feature/BubbleSort.vue
+++ src/resources/js/feature/BubbleSort.vue
@@ -12,7 +12,6 @@ const inputValidError = ref(true);
  * 入力、結果の初期化と結果の非表示を行う。
  */
 const resetButtonClick = () => {
-    console.log('reset');
      isOutputDisplayed.value = false;
     input.value = ["","","",""];
 }
@@ -21,7 +20,7 @@ const resetButtonClick = () => {
  * バブルソートの実行と結果の表示を行う。
  */
 const calculateButtonClick = () =>{
-    output.value = bubbleSort(input.value);
+    output.value = bubbleSort(input.value.map(Number));
     console.log(output.value);
     isOutputDisplayed.value = true;
 }
@@ -33,22 +32,38 @@ const calculateButtonClick = () =>{
  * @return {Array} - ソート後の配列
  */
 function bubbleSort(array){
-    console.debug(array.length);
-    for(let i = 0; i < array.length; i++){
-        for(let j = 0; j < array.length - 1; j++){
-            if(array[j] > array[j+1]){
-                let tmp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = tmp;
+    let tempArray = array.map(Number)
+    for(let i = 0; i < tempArray.length; i++){
+        for(let j = 0; j < tempArray.length - 1; j++){
+            if(tempArray[j] > tempArray[j+1]){
+                let tmp = tempArray[j];
+                tempArray[j] = tempArray[j+1];
+                tempArray[j+1] = tmp;
             }
         }
     }
-    return array;
+    return tempArray;
 }
 
 const validateInput = (array) => {
 
 }
+
+
+ const sum = (i) =>{
+
+    return i*i;
+}
+
+defineExpose({
+    isOutputDisplayed,
+    sum,
+    bubbleSort,
+    calculateButtonClick
+})
+
+</script>
+<script>
 
 </script>
 
@@ -57,34 +72,34 @@ const validateInput = (array) => {
 
 
         <v-sheet class="d-flex justify-center mb-6 ">
-            <!-- <label for="elementNumber">配列sの数</label>
+            <label for="elementNumber">配列sの数</label>
             <div width="50px" height="60px">
                 <v-select name="elementNumber" :items="['4','5','6','7','8']"></v-select>
-            </div> -->
+            </div>
         </v-sheet>
         <div>
             <div v-if="inputValidError" class="justify-center py-3 d-flex">エラーです</div>
                 <v-row class="d-flex justify-center mb-6  flex-row " >
-                        <v-text-field  maxlength=3  max-width="100px" class="px-3" v-model="input[0]"></v-text-field>
-                        <v-text-field  maxlength=3  max-width="100px" class="px-3" v-model="input[1]"></v-text-field>
-                        <v-text-field  maxlength=3  max-width="100px" class="px-3" v-model="input[2]"></v-text-field>
-                        <v-text-field  maxlength=3  max-width="100px" class="px-3" v-model="input[3]"></v-text-field>
+                        <v-text-field  maxlength=3  max-width="100px" class="px-3" v-model="input[0]" id="input_1"></v-text-field>
+                        <v-text-field  maxlength=3  max-width="100px" class="px-3" v-model="input[1]" id="input_2"></v-text-field>
+                        <v-text-field  maxlength=3  max-width="100px" class="px-3" v-model="input[2]" id="input_3"></v-text-field>
+                        <v-text-field  maxlength=3  max-width="100px" class="px-3" v-model="input[3]" id="input_4"></v-text-field>
                 </v-row>
         </div>
         <div class="d-flex justify-center mb-6  flex-row ">
-            <v-btn  class="mx-4" @click = "calculateButtonClick" >計算</v-btn>
-            <v-btn  @click = "resetButtonClick" class="mx-4" >リセット</v-btn>
+            <v-btn  class="mx-4" @click = "calculateButtonClick" id="submit" >計算</v-btn>
+            <v-btn  @click = "resetButtonClick" class="mx-4" id="reset" >リセット</v-btn>
         </div>
         <h1  class="d-flex justify-center mb-6  flex-row ">結果</h1>
         <div v-if="isOutputDisplayed">
                 <v-row class="d-flex justify-center mb-6  flex-row " >
-                        <v-text-field  maxlength=3  max-width="100px" class="px-3" :value="output[0]" :Disabled="true"></v-text-field>
-                        <v-text-field  maxlength=3  max-width="100px" class="px-3" :value="output[1]" :Disabled="true"></v-text-field>
-                        <v-text-field  maxlength=3  max-width="100px" class="px-3" :value="output[2]" Disabled="true"></v-text-field>
-                        <v-text-field  maxlength=3  max-width="100px" class="px-3" :value="output[3]" Disabled="true"></v-text-field>
+                        <v-text-field  maxlength=3  max-width="100px" class="px-3" :value="output[0]" :Disabled="true" id="output_1"></v-text-field>
+                        <v-text-field  maxlength=3  max-width="100px" class="px-3" :value="output[1]" :Disabled="true" id="output_2"></v-text-field>
+                        <v-text-field  maxlength=3  max-width="100px" class="px-3" :value="output[2]" Disabled="true" id="output_3"></v-text-field>
+                        <v-text-field  maxlength=3  max-width="100px" class="px-3" :value="output[3]" Disabled="true" id="output_4"></v-text-field>
                 </v-row>
                 <h1 class="d-flex justify-center mb-6  flex-row ">途中結果 commingsoon</h1>
-            </div>
+        </div>
 </template>
 
 
