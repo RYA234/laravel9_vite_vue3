352f383d180f3ce48c220a33c3bc726e30a57a9a
RYA234 <103587568+RYA234@users.noreply.github.com>
2024-08-08 12:10:33 +0000

    コインチェック　ロジック部分実装

--- src/resources/js/feature/greedyLaw/CoinCheck.test.js
+++ src/resources/js/feature/greedyLaw/CoinCheck.test.js
@@ -16,8 +16,7 @@ const vuetify = createVuetify({
 
 global.ResizeObserver = require('resize-observer-polyfill')
 
-test('アルゴリズムの単体ddテスト', () => {
-
+test('硬貨の問題単体テスト 620円を満たす少ない硬貨の枚数。[1,5,10,50,100,500]円= [3,2,1,3,0,2]', () => {
   const wrapper = mount(CoinCheck,
     {
     global: {
@@ -26,5 +25,4 @@ test('アルゴリズムの単体ddテスト', () => {
   })
   //   console.log(wrapper.html())
   expect(wrapper.vm.check(620,[3,2,1,3,0,2])).toBe(6);
-  
 })
--- src/resources/js/feature/greedyLaw/CoinCheck.vue
+++ src/resources/js/feature/greedyLaw/CoinCheck.vue
@@ -5,24 +5,25 @@
 <script setup>
 import { ref } from 'vue';
 
-    // コインの金額
 
-
-    //コインチェック
-    /**
-     * @
+    /**  コインチェックpp43
+     *
      * @param sum{Number} 金額の合計額
+     * @param coinNumbers{Number} 各コインの枚数 要素数は6 1番目は1円、2番目は5円,3番目は10円,4番目は50円,5番目は100円,6番目は500円
+     * @return {Number} コインの最小枚数
      */
-
-    //  let coinNumbers = new Array[5]
     const  check = (sum,coinNumbers) =>{
         const coinValue = [1,5,10,50,100,500];
-        let ans =0;
+        let ans = 0;
         let tempSum = sum;
+        console.log(`貪欲法 コインチェック 状態遷移図`);
+        console.log(`i|tempSum|ans`);
+        console.log(`:--:|:---:|:-----:`);
         for(let i=5;i > 0; i--){
             let tempNumber = Math.min(Math.floor(tempSum/coinValue[i]),coinNumbers[i])
-           tempSum -= tempNumber * coinValue[i]
+            tempSum -= tempNumber * coinValue[i]
             ans += tempNumber
+            console.log(`${i} | ${tempSum} | ${ans}`);
         }
         return ans;
     }
