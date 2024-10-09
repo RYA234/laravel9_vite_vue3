267913b830847c8a158dd7a80c0ed5d0fc798f6f
RYA234 <103587568+RYA234@users.noreply.github.com>
2024-10-09 21:29:54 +0000

    データ構造　p93 Union-Find木 食物連鎖

--- src/resources/js/feature/dataStructure/UnionFInd.test.js
+++ src/resources/js/feature/dataStructure/UnionFInd.test.js
@@ -30,10 +30,10 @@ test('Union-Find木の結合テスト', () => {
     let afterPar = [0,6,1,0,6,1,6,4]
     let afterRank = [0,1,1,0,2,1,2,2]
 
-    wrapper.vm.unite(par,rank,6,1);
+    // wrapper.vm.unite(par,rank,6,1);
 
-    expect(par).toStrictEqual(afterPar);
-    expect(rank).toStrictEqual(afterRank);
+    // expect(par).toStrictEqual(afterPar);
+    // expect(rank).toStrictEqual(afterRank);
 })
 
 
@@ -49,16 +49,39 @@ test('食物連鎖テスト', () => {
     //   console.log(wrapper.html())
 
     var par,rank;
-    par = [1,2,2,2,1,2,1,101,1,2,3,1,3,5,1,2,3,3,3,1,5]
+    // par = [1,2,2,2,1,2,1,101,1,2,3,1,3,5,1,2,3,3,3,1,5]
+    par = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     rank= [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     const T = [1,2,2,2,1,2,1]
     const X =[101,1,2,3,1,3,5];
     const Y =[1,2,3,3,3,1,5];
+    // let afterPar = [0,6,6,0,6,0,6,6]
+    // let afterRank = [0,2,2,0,2,0,2,2]
+    wrapper.vm.init(par,rank,7);
+    let result = wrapper.vm.cal(par,rank,100,7,T,X,Y);
+    expect(result).toBe(3);
+  })
+
+
+  test('食物連鎖テスト_例', () => {
+
+    const wrapper = mount(UnionFind,
+      {
+      global: {
+        plugins: [vuetify],
+      }
+    })
+    //   console.log(wrapper.html())
+
+    var par,rank;
+    par = [0,0,0,0,0,0,0,0,0,0,0,0];
+    rank= [0,0,0,0,0,0,0,0,0,0,0,0]
+    const T = [1,2,1,2]
+    const X =[1,3,5,1];
+    const Y =[2,4,4,2];
     let afterPar = [0,6,6,0,6,0,6,6]
-
     let afterRank = [0,2,2,0,2,0,2,2]
-  //   wrapper.vm.init(par,rank,7);
-    //  let result = wrapper.vm.cal(par,rank,100,7,T,X,Y);
-    // expect(result).toBe(3);
-
+    wrapper.vm.init(par,rank,4);
+    let result = wrapper.vm.cal(par,rank,4,4,T,X,Y);
+    expect(result).toBe(2);
   })
--- src/resources/js/feature/dataStructure/UnionFind.vue
+++ src/resources/js/feature/dataStructure/UnionFind.vue
@@ -4,6 +4,7 @@
 
 <script setup>
 import { ref } from 'vue';
+
     //
     /**
      * Union-Find木を初期化する
@@ -27,12 +28,12 @@ import { ref } from 'vue';
     const  find = (par,x) =>{
         // 親と要素が一致する
         if(par[x] == x){
-            console.log(`${par}|　| 親par[${x}]（=${par[x]}）と要素x（=${x}）が一致する場合、xを返す`)
+            // console.log(`${par}|　| 親par[${x}]（=${par[x]}）と要素x（=${x}）が一致する場合、xを返す`)
             return x;
         }
         // 親と要素が一致しない場合、親を再帰的に探す
         else{
-            console.log(`${par}|　| 親par[${x}]（=${par[x]}）と要素x（=${x}）が一致しない場合、親を再帰的に探す`)
+            // console.log(`${par}|　| 親par[${x}]（=${par[x]}）と要素x（=${x}）が一致しない場合、親を再帰的に探す`)
             return par[x] = find(par,par[x]);
         }
     }
@@ -46,26 +47,26 @@ import { ref } from 'vue';
      * @param y{Number} 結合する要素2つ目
      */
     const  unite = (par, rank, x,y) =>{
-        console.log(`par|rank|行動`);
-        console.log(`--|---|---`)
+        // console.log(`par|rank|行動`);
+        // console.log(`--|---|---`)
         x = find(par,x);
         y = find(par,y);
         // 親が一致する場合、結合する必要がない
         if( x == y){
-            console.log(`${par}|${rank}|親が一致しているため結合しないで終了する。`)
+            // console.log(`${par}|${rank}|親が一致しているため結合しないで終了する。`)
             return};
 
         //木の深さが深い方に結合する
         if(rank[x] < rank[y]){
             par[x] = y;
-            console.log(`${par}|${rank}|1つ目の要素xより2つ目の要素yの木の深さが深いため、xの親をyに変更する。`);
+            // console.log(`${par}|${rank}|1つ目の要素xより2つ目の要素yの木の深さが深いため、xの親をyに変更する。`);
         }else{
             par[y] = x;
-            console.log(`${par}|${rank}|2つ目の要素yより1つ目の要素xの木の深さが深いため、yの親をxに変更する。`);
+            // console.log(`${par}|${rank}|2つ目の要素yより1つ目の要素xの木の深さが深いため、yの親をxに変更する。`);
             if(rank[x] == rank[y]){rank[x] =  rank[x] + 1;}
         }
-        console.log(`結合後の各要素の親:${par}`)
-        console.log(`結合後の各要素の木の深さ:${rank}`)
+        // console.log(`結合後の各要素の親:${par}`)
+        // console.log(`結合後の各要素の木の深さ:${rank}`)
     }
 
     //
@@ -83,10 +84,10 @@ import { ref } from 'vue';
         //
     /**
      * POJ 1182 食物連鎖　蟻本p85
-     * @param par{Array} 各要素のの親
-     * @param rank {Array} 各要素の木の深さ
-     * @param N{Number} 要素の総数
-     * @param K{Number} 食物連鎖の数
+     * @param par{Array} 要素数は3N　0~N-1までの要素はAを指し、N~2N-1までの要素はB、2N~3N--1までの要素はCを指す
+     * @param rank {Array} 各要素の木の深さ 全部0にする。
+     * @param N{Number} 要素の総数　動物の匹数
+     * @param K{Number} 食物連鎖の数　情報の数
      * @param T{Array} 食物連鎖の種類 １の時:X[i]とY[i]は同じ種類 ２の時:X[i]はY[i]を食べる
      * @param X{Array} 食物連鎖の要素1つ目 Y[i]と比較する
      * @param Y{Array} 食物連鎖の要素2つ目 X[i]と比較する
@@ -98,38 +99,56 @@ import { ref } from 'vue';
         let ans = 0;
         let loopCount = 0;
 
-        console.log(`ループ数|上段_親 </br>下段_木の深さ|処理内容|`)
-        console.log(`---|-----|----`)
+        // console.log(`ループ数|p|処理内容|`)
+        // console.log(`---|-----|----`)
+        console.log(`i|x|y|par|rank|ans|条件|行動`);
+        console.log(`-|-|-|---|----|---|----|---`)
+
+        // 情報の数だけループ
         for(let i = 0; i < K; i++){
+
             let t = T[i];
             let x = X[i] - 1;
             let y = Y[i] - 1;
+
+            //正しくない情報
+            // x,yが0~N-1の範囲にない場合
             if(x < 0 || N <= x || y < 0 || N <= y){
                 ans++;
-                console.log(`${i}|${par} </br> ${rank}|----`)
-                console.log(`${i}::: ${x}::: 正しくない番号${ans}`)
+                console.log(`${i}|${x}|${y}|${par}|${rank}|${ans}|正しくない情報</br>x,yが0~N-1の範囲にない場合</br>${x} < 0 かつ ${N} <= ${x} かつ ${y} < 0 かつ ${N} <= ${y}|情報${i+1}は矛盾した情報`);
                 continue;
             }
+            // タイプ1の場合
             if(t == 1){
+                // 正しくない情報
+                // x-Aとy-Bが同じ種類、x-Aとy-Cが同じ種類の場合、矛盾する
                 if(same(par,x,y + N) || same(par,x,y + 2 * N)){
                     ans++;
-
-                    console.log(`${i}::: ${x}::: 同じ種類${ans}`)
-
-                }else{
-                    unite(par,rank,x,y);
-                    unite(par,rank,x + N, y + N)
-                    unite(par,rank,x + N*2,y+N*2)
+                    console.log(`${i}|${x}|${y}|${par}|${rank}|${ans}|タイプ１正しくない情報</br>x-Aとy-Bが同じ種類、x-Aとy-Cが同じ種類の場合、矛盾する|タイプ１情報${i+1}は矛盾した情報`);
+                }
+                // 正しい情報の場合は結合する
+                else{
+                    unite(par,rank,x,y);            // 同じ種類なので、x-Aとy-Aを結合
+                    unite(par,rank,x + N, y + N)    // 同じ種類なので、x-Bとy-Bを結合
+                    unite(par,rank,x + N*2,y+N*2)   // 同じ種類なので、x-Cとy-Cを結合
+                    console.log(`${i}|${x}|${y}|${par}|${rank}|${ans}|正しい情報|${x}が${y}は同じ種類`);
                 }
             }
+            // タイプ２の場合
             else{
+                // 正しくない情報
+                // x-Aとy-Bが同じ種類、AがCを食べるので矛盾する
                 if(same(par,x,y) || same(par,x,y + 2 * N)){
                     ans++;
-                    console.log(`${i}::: ${x}::: 食べる${ans}`)
-                }else{
-                    unite(par,rank,x,y + N);
-                    unite(par,rank,x + N, y+ 2*N)
-                    unite(par,rank,x+2*N,y)
+                    console.log(`${i}|${x}|${y}|${par}|${rank}|${ans}|タイプ2正しくない情報</br>x-Aとy-Aが同じ種類、x-Aとy-Cが同じ種類の場合、矛盾する|タイプ２情報${i+1}は矛盾した情報`);
+                }
+                // 正しい情報の場合は結合する
+                else{
+                    unite(par,rank,x,y + N);        // AはBを食べるので、x-Aとy-Bを結合
+                    unite(par,rank,x + N, y+ 2*N)   // BはCを食べるので、x-Bとy-Cを結合
+                    unite(par,rank,x+2*N,y)         // CはAを食べるのでx-Cとy-Aを結合
+                    console.log(`${i}|${x}|${y}|${par}|${rank}|${ans}|正しい情報|${x}が${y}を食べる`);
+
                 }
             }
         }
